511d1f179f77e0545033ac748e979f95e48e852b
Sanjiv Pradhanang <pradhanang.sanjiv@gmail.com>
2018-06-16 18:05:01 -0500

    Grid drawing under way

---
+++ PixelArtMaker/designs.js
@@ -0,0 +1,21 @@
+const colorPicker = document.querySelector('#colorPicker');
+// Select size input
+const height = document.querySelector('#inputHeight');
+const width = document.querySelector('#inputWidth');
+// When size is submitted by the user, call makeGrid()
+const form = document.getElementById('sizePicker');
+
+function makeGrid() {
+  const table = document.getElementById('pixelCanvas');
+  table.setAttribute("style", "color:red; border: 1px solid blue;");
+  for (let r=0; r<height.value; r++){
+    const row = document.createElement('tr');
+    table.appendChild(row);
+    for (let c=0; c<width.value; c++){
+       const cell = document.createElement('td');
+       row.appendChild(cell);
+    }
+  }
+}
+
+form.addEventListener('submit', makeGrid);
